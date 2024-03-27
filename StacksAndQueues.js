224cc1110a63df9f281a383a8b79d3f01e91bcd3
Spade <77303844+11Spades@users.noreply.github.com>
2024-03-27 00:11:02 -0400

    Make input actually block.

--- StacksAndQueues.js
+++ StacksAndQueues.js
@@ -1,4 +1,6 @@
 const readline = require('readline');
+const { stdin: input, stdout: output } = require("node:process");
+const rl = readline.createInterface({ input, output });
 
 class Queue {
     constructor() {
@@ -32,35 +34,37 @@ class Stack {
     }
 }
 
-// Set up I/O
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-// Create and populate the queue
-var queue = new Queue();
-for (i = 0; i < 9; ++i) {
-    queue.push(i);
+function ask(text) {
+    return new Promise(resolve => rl.question(text, answer => {
+        resolve(answer);
+    }));
 }
 
-// Create and populate the stack
-var stack = new Stack();
-for (i = 0; i < 10; ++i) {
-    stack.push(i);
+const main = async () => {
+    // Create and populate the queue
+    var queue = new Queue();
+    for (i = 0; i < 9; ++i) {
+        queue.push(i);
+    }
+
+    // Create and populate the stack
+    var stack = new Stack();
+    for (i = 0; i < 10; ++i) {
+        stack.push(i);
+    }
+
+    // Start the loop!
+    while (true) {
+        console.log("The queue currently contains the following data:\n");
+        console.log(queue.data);
+        console.log();
+        console.log("The stack currently contains the following data:\n");
+        console.log(stack.data);
+        console.log();
+        await ask("Please enter anything to continue.");
+        queue.push(stack.pop());
+        stack.push(queue.pop_front());
+    }
 }
 
-// Start the loop!
-while (true) {
-    console.log("The queue currently contains the following data:\n");
-    console.log(queue.data);
-    console.log();
-    console.log("The stack currently contains the following data:\n");
-    console.log(stack.data);
-    console.log();
-    rl.question("Please enter anything to continue.", answer => {
-        rl.close();
-    });
-    queue.push(stack.pop());
-    stack.push(queue.pop_front());
-}
+main();
